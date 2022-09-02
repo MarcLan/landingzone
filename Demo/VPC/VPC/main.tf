bf324abe4e4d34ca1632b743d342421303495eac
= <marclan8728@gmail.com>
2022-09-02 16:24:59 +0700

    correct resource

--- Demo/VPC/VPC/main.tf
+++ Demo/VPC/VPC/main.tf
@@ -1,9 +1,9 @@
-resource "hauweicloud_vpc" "vpc_landingzone" {
+resource "huaweicloud_vpc" "vpc_landingzone" {
   name = var.vpc_name
   cidr = var.vpc_cidr
 }
 
-resource "hauweicloud_vpc_subnet" "subnet_landingzone" {
+resource "huaweicloud_vpc_subnet" "subnet_landingzone" {
   vpc_id = huaweicloud_vpc.vpc_landingzone.id
   name = var.subnet_name
   cidr = var.subnet_cidr
