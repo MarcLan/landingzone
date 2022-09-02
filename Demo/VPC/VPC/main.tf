bb707d2caef32c07ef45c293a6aab4fddb3bb3ba
= <marclan8728@gmail.com>
2022-09-02 15:13:52 +0700

    add

---
+++ Demo/VPC/VPC/main.tf
@@ -0,0 +1,10 @@
+resource "hauweicloud_vpc" "vpc_landingzone" {
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+}
+
+resource "hauweicloud_vpc_subnet" "subnet_landingzone" {
+  vpc_id = huaweicloud_vpc.vpc_landingzone.id
+  name = var.subnet_name
+  cidr = var.subnet_cidr
+}
